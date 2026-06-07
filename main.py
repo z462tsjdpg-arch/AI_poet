1a0885756da8c690d98ebf430259cb9768643890
z462tsjdpg-arch <z462tsjdpg@privaterelay.appleid.com>
2026-06-07 21:23:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-from langchain_openai import ChatOpenAI
+import os
 import streamlit as st
-
-chat_model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+from langchain_openai import ChatOpenAI
 
 st.title("인공지능 시인")
 
@@ -12,5 +8,9 @@ subject = st.text_input("시의 주제를 입력해 주세요")
 
 if st.button("시 작성"):
     with st.spinner("시 작성 중..."):
+        chat_model = ChatOpenAI(
+            model="gpt-3.5-turbo",
+            api_key=os.environ.get("OPENAI_API_KEY")
+        )
         result = chat_model.invoke(subject + "에 대한 시를 써줘")
         st.write(result.content)
